aedfd46924305095d002abec3ca87743db998078
pointlesspun <anotherpointlesspun@gmail.com>
2026-04-29 22:59:29 +0200

    refactor: move over to new data structure for locations

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { cloudflare } from "@cloudflare/vite-plugin";
+import yaml from '@modyfi/vite-plugin-yaml'
 
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react(), cloudflare(), yaml()],
   server: {
     historyApiFallback: true,
   },
